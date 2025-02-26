78a2b4fa52ffdcbbbf6436c7b2c0eb21ffcde724
Tohid Khan <tohidkhan9050482152@gmail.com>
2025-02-26 19:15:36 +0530

    TOHID-AI

--- gifted/general/help.js
+++ gifted/general/help.js
@@ -1,1 +1,16 @@
+let Giftedd = async (m, { Gifted }) => {
 
+    let giftedButtons = [
+          [
+            { text: 'WaChannel', url: 'https://whatsapp.com/channel/0029VaGyP933bbVC7G0x0i2T' }
+        ]
+    ];
+
+    Gifted.reply({ text: `Need help? Kindly Consult my Owner: @${global.ownerUsername}\nEspecially incase a Command is not Working` }, giftedButtons, m)
+}
+
+Giftedd.command = ['help']
+Giftedd.desc = 'Get Help'
+Giftedd.category = ['general']
+
+module.exports = Giftedd
