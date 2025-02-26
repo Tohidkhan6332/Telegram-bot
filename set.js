970f3e0cefcc3a971a6e4f9d332290b9e5346a64
Tohid Khan <tohidkhan9050482152@gmail.com>
2025-02-26 19:26:44 +0530

    TOHID-AI

---
+++ set.js
@@ -0,0 +1,30 @@
+global.prefix = '/'; // Set Your Preferred Prefix
+global.botName = 'TOHID-AI'; // Bot Name
+global.botVersion = '2.0.0';
+global.botPic = 'https://i.imgur.com/WnKjrJt.jpeg'; // Can use path, can also use link
+global.botToken = 'xxxxxxxx:xxxx'; // Replace With Your Bot Token (Get it from BotFather)
+global.ownerUsername = '@tohid_mewati'; // Replace With Your Telegram Username(For Bot Owner)
+global.timeZone = 'Aesia/India'; // Your Time Zone
+global.ownerId = [xxxxxx]; // Replace With Your Owner ID(For Bot Owner), You Can Add More than 1
+global.footer = '*Powered by TohidKhan*';
+global.ytdlWeb = 'https://ytdl.giftedtech.web.id';
+global.ytdlApi = 'https://giftedtech-ytdl-api.hf.space';
+global.giftedApi = 'https://api.giftedtech.web.id/api'; // Gifted Api
+global.giftedKey = ''; // Gifted Api Key
+global.maherApi = 'https://api.nexoracle.com'; // NexOracle Api
+global.maherKey = ''; // NexOracle Api Key
+global.lyffeApi = 'https://levanter.onrender.com'; // Lyffe Api
+
+global.giftechMess = {
+    internalerr: 'Internal Server Error!',
+    owner: 'Owner-Only Feature!',
+    group: 'Use in a Group!',
+    private: 'Use in a Private Chat!',
+    wait: 'Please Wait...',
+    done: 'Completed Successfully!',
+    error: 'Oops, an Error Occurred'
+};
+
+String.prototype.capitalize = function () {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+};
